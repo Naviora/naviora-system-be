a29cccb5d733598776b854e0ea29e4c42d8e938c
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2025-10-02 21:23:47 +0700

    feat(class): add api property decorator [WDP301-38]

--- src/api/class/dto/create-class.dto.ts
+++ src/api/class/dto/create-class.dto.ts
@@ -1,24 +1,45 @@
 import { ClassType } from '@common/enums/class-types.enum'
+import { ApiProperty } from '@nestjs/swagger'
 import { IsDate, IsEnum, IsNotEmpty, Matches } from 'class-validator'
 
 export class CreateClassDto {
+  @ApiProperty({
+    description: 'The code of the class',
+    example: 'BIO-25-001'
+  })
   @IsNotEmpty()
   @Matches(/^BIO-\d{2}-\d{3}$/i, {
     message: 'classCode must match format BIO-YY-NNN (e.g., BIO-25-001)'
   })
   classCode: string
 
+  @ApiProperty({
+    description: 'The name of the class',
+    example: 'Biology Class 1'
+  })
   @IsNotEmpty()
   className: string
 
+  @ApiProperty({
+    description: 'The type of the class - it indicates the scope of the competition in which students participate',
+    example: 'city'
+  })
   @IsNotEmpty()
   @IsEnum(ClassType)
   classType: ClassType
 
+  @ApiProperty({
+    description: 'The start date of the class',
+    example: '2025-01-01'
+  })
   @IsNotEmpty()
   @IsDate()
   startDate: Date
 
+  @ApiProperty({
+    description: 'The end date of the class',
+    example: '2025-01-01'
+  })
   @IsNotEmpty()
   @IsDate()
   endDate: Date
