e3e75a044ee79ae6323df2b0a2e1dba1bd9b7ffb
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2025-10-19 17:44:32 +0700

    chore(material): fix lint

--- src/api/teaching-material/teaching-material.controller.spec.ts
+++ src/api/teaching-material/teaching-material.controller.spec.ts
@@ -1,20 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { TeachingMaterialController } from './teaching-material.controller';
-import { TeachingMaterialService } from './teaching-material.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { TeachingMaterialController } from './teaching-material.controller'
+import { TeachingMaterialService } from './teaching-material.service'
 
 describe('TeachingMaterialController', () => {
-  let controller: TeachingMaterialController;
+  let controller: TeachingMaterialController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TeachingMaterialController],
-      providers: [TeachingMaterialService],
-    }).compile();
+      providers: [TeachingMaterialService]
+    }).compile()
 
-    controller = module.get<TeachingMaterialController>(TeachingMaterialController);
-  });
+    controller = module.get<TeachingMaterialController>(TeachingMaterialController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
