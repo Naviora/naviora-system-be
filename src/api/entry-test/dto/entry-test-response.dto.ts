8a3622dba3e0b0a8de523f30b59e35ce5f86dc68
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2025-11-10 08:05:13 +0700

    chore(entry-test): response basic question set info

--- src/api/entry-test/dto/entry-test-response.dto.ts
+++ src/api/entry-test/dto/entry-test-response.dto.ts
@@ -2,6 +2,7 @@ import { User } from '@api/user/entities/user.entity'
 import { ExamStatus } from '@common/enums/exam-status.enum'
 import { ApiProperty } from '@nestjs/swagger'
 import { Expose } from 'class-transformer'
+import { QuestionSetBasicInfoDto } from './question-set-basic-info.dto'
 
 export class EntryTestResponseDto {
   @ApiProperty()
@@ -28,9 +29,9 @@ export class EntryTestResponseDto {
   @Expose()
   endTime: Date
 
-  @ApiProperty({ type: [String] })
+  @ApiProperty({ type: [QuestionSetBasicInfoDto] })
   @Expose()
-  questionSets: string[]
+  questionSets: QuestionSetBasicInfoDto[]
 
   @ApiProperty()
   @Expose()
