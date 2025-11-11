9f5cafeb33051d2b77c9c1cfcb51ae56d4952d92
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2025-11-11 13:36:00 +0700

    feat(modules): add filter and get modules within assigned class

--- src/api/module/dto/get-modules-query.dto.ts
+++ src/api/module/dto/get-modules-query.dto.ts
@@ -1,7 +1,10 @@
 import { PageOptionsDto } from '@common/dto/offset-pagination/page-options.dto'
-import { StringFieldOptional } from '@decorators/field.decorators'
+import { StringFieldOptional, UUIDFieldOptional } from '@decorators/field.decorators'
 
 export class GetModulesQueryDto extends PageOptionsDto {
   @StringFieldOptional()
   readonly sort_by?: string
+
+  @UUIDFieldOptional({ description: 'Filter modules by class ID' })
+  readonly class_id?: string
 }
